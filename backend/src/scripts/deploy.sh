725db547d45e301c10f269fb8f77c3c9927259e2
Yacine Badiss <yacine.badiss@gmail.com>
2025-04-13 17:54:55 +0200

    fix ssh keys

--- backend/src/scripts/deploy.sh
+++ backend/src/scripts/deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-ssh-add ~/.ssh/DigitalOcean
-
 # Build the project
 npm run build
 
 timestamp=$(date +%s)
 
 # Deploy the project
-scp -i ~/.ssh/DigitalOcean -r dist/* root@167.71.143.97:/var/www/recip.it/backend-${timestamp}
-scp -i ~/.ssh/DigitalOcean package*.json root@167.71.143.97:/var/www/recip.it/backend-${timestamp}/
-ssh -i ~/.ssh/DigitalOcean root@167.71.143.97 "ln -s /root/recip.it/backend/.env /var/www/recip.it/backend-${timestamp}/.env"
+scp -r dist/* root@167.71.143.97:/var/www/recip.it/backend-${timestamp}
+scp package*.json root@167.71.143.97:/var/www/recip.it/backend-${timestamp}/
+ssh root@167.71.143.97 "ln -s /root/recip.it/backend/.env /var/www/recip.it/backend-${timestamp}/.env"
 
 # Install dependencies
-ssh -i ~/.ssh/DigitalOcean root@167.71.143.97 "cd /var/www/recip.it/backend-${timestamp} && npm i --omit=dev"
+ssh root@167.71.143.97 "cd /var/www/recip.it/backend-${timestamp} && npm i --omit=dev"
 
 # Create a new symlink
-ssh -i ~/.ssh/DigitalOcean root@167.71.143.97 "rm /var/www/recip.it/backend && ln -s /var/www/recip.it/backend-${timestamp} /var/www/recip.it/backend"
-ssh -i ~/.ssh/DigitalOcean root@167.71.143.97 "pm2 restart recipi-backend && sleep 1 && pm2 logs recipi-backend"
+ssh root@167.71.143.97 "rm /var/www/recip.it/backend && ln -s /var/www/recip.it/backend-${timestamp} /var/www/recip.it/backend"
+ssh root@167.71.143.97 "pm2 restart recipi-backend && sleep 1 && pm2 logs recipi-backend"
--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-ssh-add ~/.ssh/DigitalOcean
-
 # Build the project
 VITE_API_URL=https://api.recipit.me npm run build
 
 timestamp=$(date +%s)
 
 # Deploy the project
-scp -i ~/.ssh/DigitalOcean -r dist/* root@167.71.143.97:/var/www/recip.it/frontend-${timestamp}
-scp -i ~/.ssh/DigitalOcean package*.json root@167.71.143.97:/var/www/recip.it/frontend-${timestamp}/
+scp -r dist/* root@167.71.143.97:/var/www/recip.it/frontend-${timestamp}
+scp package*.json root@167.71.143.97:/var/www/recip.it/frontend-${timestamp}/
 
 # Install dependencies
-ssh -i ~/.ssh/DigitalOcean root@167.71.143.97 "cd /var/www/recip.it/frontend-${timestamp} && npm i --omit=dev"
+ssh root@167.71.143.97 "cd /var/www/recip.it/frontend-${timestamp} && npm i --omit=dev"
 
 # Create a new symlink
-ssh -i ~/.ssh/DigitalOcean root@167.71.143.97 "rm /var/www/recip.it/frontend && ln -s /var/www/recip.it/frontend-${timestamp} /var/www/recip.it/frontend"
+ssh root@167.71.143.97 "rm /var/www/recip.it/frontend && ln -s /var/www/recip.it/frontend-${timestamp} /var/www/recip.it/frontend"
